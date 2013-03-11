44fc9ebb4bf4072a256f0ea519ccef9c86bb4664
zaqwes <igor.a.lugansky@gmail.com>
2013-03-11 21:08:32 +0400

    Adapt source model.

--- amplifier_model.py
+++ amplifier_model.py
@@ -3,49 +3,60 @@ from pylab import *
 from numpy import *
 from scipy.optimize import leastsq
 
-# Parametric function: 'v' is the parameter vector, 
-#    'x' the independent varible
-def fp(v, x):
-    return v[0]/(x**v[1])*sin(v[2]*x)  # Sinc(x)
+def real_relation(v, x):
+    """ Известная зависимость 
     
+    Args: 
+        Параметры кривой
+        Ось Ox
+    
+    Returns:
+        Массив отсчетов y = real_relation(v, x)
+    """
+    y = v[0]*v[1]*sin(v[2]*x)
+    return  y 
 
-## Noisy function (used to generate data to fit)
-v_real = [1.5, 0.1, 2.]
-def fn(x, v_real): 
-    return fp(v_real, x)
-
-## Error function
-#e = lambda v, x, y:   # **2
 def e(v, x, y):
-    return (fp(v,x)-y)
+    """ Error function. Очень важная. """
+    return (real_relation(v,x)-y)
 
-## Generating noisy data to fit
-n = 30
-xmin = 0.1
-xmax = 5
-x = linspace(xmin,xmax,n)
+def get_ox(num_points):
+    n = num_points
+    xmin = 0.1
+    xmax = 5
+    x = linspace(xmin,xmax,n)
+    return x
 
-# Зашупленная функция
-y = fn(x, v_real)+rand(len(x))*0.2*(fn(x, v_real).max()-fn(x, v_real).min())
-
-## Initial parameter value
-v0 = [3., 1, 4.]
-
-
-## Fitting
-v, success = leastsq(e, v0, args=(x,y), maxfev=10000)
-
-## Plot
-def plot_fit():
-    print 'Estimater parameters: ', v
-    print 'Real parameters: ', v_real
-    X = linspace(xmin,xmax,n*5)
-    plot(x,y,'ro', X, fp(v,X))
-
-plot_fit()
-show()
+def main():
+    # Plot
+    def plot_fit(nun_points):
+        print 'Estimater parameters: ', v
+        print 'Real parameters: ', v_real
+        x_precise = get_ox(nun_points)
+        plot(x, y,'ro', x_precise, real_relation(v, x_precise))
+    
+    # Поучаем ось
+    n = 50
+    x = get_ox(n)
+    
+    # Параметры функции и начальная точка поиска
+    v0 = [1.5, .1, 2.3]  # Initial parameter value
+    v_real = [1.5, 0.1, 2.]  # Реальная наша функция
+    #v0 = v_real  # TODO(you): взять поближеа
+    
+    # Зашупленная функция
+    for i in range(10):
+        sigma = 0.01
+        noise = random.normal(0, sigma, size=n)
+        y = real_relation(v_real, x)+noise
+        v, success = leastsq(e, v0, args=(x,y), maxfev=10000)
+        plot_fit(n*5)
+    
+    #
+    show()
 
 if __name__=="__main__":
+    main()
     pass
 
 
