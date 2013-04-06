89296d12aeae816b40407e65c24f551a52f1f23e
Your Name <you@example.com>
2013-04-07 00:20:55 +0400

    'fix'

--- src/iir_models/second_try.py
+++ src/iir_models/second_try.py
@@ -8,10 +8,9 @@ from pylab import plot
 from pylab import subplot
 from pylab import show
 from pylab import grid
+
 from numpy import ndarray
 from numpy import arange
-
-
 from numpy import angle
 from numpy import conj
 from numpy import real
@@ -20,8 +19,7 @@ from numpy import exp
 from numpy import pi
 
 def plot_AFC(w, T1):
-    y = 1/(1+w*T1)
-    y = real(conj(y)*y)**0.5
+      y = 
     return y
 
 def plot_PFC(w, T1):
@@ -30,29 +28,16 @@ def plot_PFC(w, T1):
     return y
     # pass
 
-def main(w, w_full):
-    #run   
-    T1 = .01
-    K = 1#0
-    x = w
-    y1 = plot_AFC(x, T1)  
-    subplot(2, 1, 1); plot(imag(w_full)/2/pi, y1*K); grid()
-    y2 = plot_PFC(x, T1)
-    subplot(2, 1, 2); plot(imag(w_full)/2/pi, y2); grid();
-    show()  
+#run   
+T1 = .01
+K = 1#0
+x = arange(100)/100.0
+y1 = plot_AFC(x, T1)  
+subplot(2, 1, 1); plot(x, y1); grid()
+y2 = plot_PFC(x, T1)
+subplot(2, 1, 2); plot(x, y2); grid()
+show()  
 
 # Ось должна быть e(-jwT)
  
-if __name__ == "__main__":
-    F = arange(1000)
-    w = 2*pi*F
-    Fs = 500.0  # Hz
-    T = 1/(Fs)
-    
-    z = exp(-1j*w*T)
-    print z
-    main(z, w*1j)  
-    
-    # a = 1+1j
-    #print real(conj(a)*a)**0.5
-    
+
