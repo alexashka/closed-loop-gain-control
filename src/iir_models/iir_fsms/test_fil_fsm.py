07190442c1add6e2bcf04c8be027b846d1db640a
Your Name <you@example.com>
2013-04-26 21:08:19 +0400

    git

--- src/iir_models/iir_fsms/test_fil_fsm.py
+++ src/iir_models/iir_fsms/test_fil_fsm.py
@@ -2,6 +2,9 @@
 from numpy import array
 from numpy import zeros
 from numpy import ones
+from numpy import roll
+
+from visualisers  import impz
 
 class FSMBaseIIR(object):
     a_ = None
@@ -18,15 +21,13 @@ class FSMBaseIIR(object):
         Precond.:
             a[0] = 1
         """
-        
-        print "Constr"
+
         self.b_ = b
         self.a_ = a[1:]  # Выход сумматроа не умножается ни на что!
         
-        self.x_line_ = ones(len(b)-1)
-        self.y_line_ = ones(len(self.a_))
-        
-        print self.x_line_
+        self.x_line_ = zeros(len(b)-1)
+        self.y_line_ = zeros(len(self.a_))
+
     
     def calc_response_(self, x):
         # FIR-part
@@ -40,23 +41,39 @@ class FSMBaseIIR(object):
         
         # IIR-part
         for j in range(len(self.a_)):
-             summ_line -= self.a_[j]*self.y_line_[j]
+            summ_line -= self.a_[j]*self.y_line_[j]
 
-        print summ_line
+        return summ_line
+        
+    def clk(self, x):
+        y = self.calc_response_(x)
+        self.shift_lines_(x, y)
+        return y
+        
+    def shift_lines_(self, x, y):
+        self.x_line_ = roll(self.x_line_, 1)
+        self.x_line_[0] = x
+        
+        
+        self.y_line_ = roll(self.y_line_, 1)
+        self.y_line_[0] = y
 
 
 if __name__=="__main__":
     b = [0.00694444,  0.01388889,  0.00694444]
     a = [1., -1.33333333,  0.44444444]
     
-    
-    # Fake
-    #b = [1,1,1,1,2]
-    #a = [1, 1, 1]
-    
-    x_signal = array([1,0,0,0,0,0,0,0])
+    x_signal = ones(50)
+    x_signal[0] = 0
     iir_model = FSMBaseIIR(b, a)
-    iir_model.calc_response_(x_signal[0])
+    response = []
+    for it in x_signal:
+        response.append(iir_model.clk(it))
+        
+    from pylab import *
+    plot(range(len(response)), response, 'o')
+    grid()
+    show()
     
     print 'Done'
     
