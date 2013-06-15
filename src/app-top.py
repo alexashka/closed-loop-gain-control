17c09e05af754b59304a63aae1be85241b8349fd
bb <bb@gmail.com>
2013-06-15 11:26:38 +0400

    fix

--- src/app-top.py
+++ src/app-top.py
@@ -39,76 +39,69 @@ def get_notes(curves, axis, zero_point=None):
         print v_result
         params.append(v_result)
     return params
-
+ 
 def main():
-    # Задаем начальные параметры
+    # Исходные параметры
     voltage_agc_ref = 1.2  # V - AGC - auto gain control
     dv_ref = 0.1  # V - тестовый скачек
     tau = 15.0  # оценочное врем переходный процессов
     window_metro = tau*3  # sec.
     Fs = 30.0  # freq. sampling - Hz ; with oversampling
-    num_points = window_metro*Fs
-    
-    print "num_points: ", num_points
-    axis = XAxis(num_points, 1/Fs)  # Общая временная ось
-    
-    
-    # Базовые параметры
-    count_curves = 6
-    curves = get_list_curves(axis=axis, count_curves=count_curves)
-    
-    # Оцениваем все параметры кривых
+    num_points = window_metro*Fs  # сколько отсчетов берем
+    # Задаем начальное приближение
+    # Все времена порядка секунд
     T1 = 2.0
     T2 = 1.0
     dt = 0.0
     max_dtemperature = 3.0
     temperature_ref = 70.0
     zero_point = (T1, T2, dt, max_dtemperature, temperature_ref)
+    axis = XAxis(num_points, 1/Fs)  # Общая временная ось
+
+    # Получить измеренные кривые
+    print "num_points: ", num_points
+    count_curves = 6
+    curves = get_list_curves(axis=axis, count_curves=count_curves)
+    
+    # Оцениваем все параметры кривых
     params = get_notes(curves, axis, zero_point)
     
-    # DEVELOP
-    x = axis.get_axis()
-    for curve in curves:
-        #plot(x, curve,'b')
-        pass
+    if False:
+        x = axis.get_axis()
+        for curve in curves:
+            #plot(x, curve,'b')
+            pass
+    
+        for record in params:    
+            #plot(x, wrapper_for_finding_2l_del_full(record, x),'r')
+            pass
+        #grid(); show()
 
-    for record in params:    
-        #plot(x, wrapper_for_finding_2l_del_full(record, x),'r')
-        pass
-    #grid(); show()
-
-    def mean_list_lists(list_lists):
-        count_lists = len(list_lists)
-        result = zeros(len(list_lists[0]))
-        for list_values in list_lists:
-            result += list_values
-        summary = [] 
-        for value in result:
-            summary.append(value/count_lists)
-        return array(summary)
-
-
-    mean_params = [  5.32255626,   3.07633474,   0.88892465,   2.14692147,  69.83541651]#mean_list_lists(params)
+    # Усредняем
+    if False:
+        # Рассчитываем
+        mean_params  = mean_list_lists(params)
+    else:
+        # Используем уже полученные результаты
+        mean_params = [  5.32255626,   3.07633474,   0.88892465,   2.14692147,  69.83541651]
     print 'mean', mean_params 
     
-    # Генерируем зашумленные данные 
+    # Оценка стойкости к зашумленности исходных данных
     
-    #get_list_curves() 
     
     # Рассчитываем незашумленную кривую
     freq_sampling = 3.0  # Hz
     if False:
         T1, T2, dt, max_dtemperature, temperature_ref = mean_params
-        #dt = 0
-        
+      
         num_points = 1024
         freq_axis = calc_half_fs_axis(num_points, freq_sampling)
         dVoltage = 0.6  # V
         params = T1, T2, dt, max_dtemperature/dVoltage, temperature_ref
         h, phi, freq_axis, h_db = calc_analog_filter_curves(
-                                                            params, 
-                                                            freq_axis, 
-                                                            af_order2_asym_delay)
+                params, 
+                freq_axis, 
+                af_order2_asym_delay)
         cut_position = get_cut_position(h_db)
             
         # Рисуем
@@ -116,7 +109,7 @@ def main():
         plot_normalize_analog(h, phi, freq_axis, freq_sampling, cut_position)
         #show()
 
-    #if True:
+    if True:
         from visualisers import mfreqz
         from visualisers import impz
         from iir_models.iir_digital import calc_digital_characteristics
