3fad3d1687ad029119a635750e43cfd4c4c872b1
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-02-07 19:42:54 +0400

    save and push

--- machine_learning/ml-exercises-mlclass-ex1-004/py_ex1/main.py
+++ machine_learning/ml-exercises-mlclass-ex1-004/py_ex1/main.py
@@ -6,6 +6,10 @@ import numpy
 def warm_up_exercise():
     return numpy.eye(5)
 
-my_data = numpy.genfromtxt('mlclass-ex1/ex1data1.txt', delimiter=',')
-print my_data
+
+def load(filename):
+    return numpy.genfromtxt(filename, delimiter=',')
+
+if __name__ == '__main__':
+    data = load('mlclass-ex1/ex1data1.txt')
 
