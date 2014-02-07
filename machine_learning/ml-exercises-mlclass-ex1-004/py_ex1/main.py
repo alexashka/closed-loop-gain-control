ce26512e905bd67b9bd613430004bf738be5fdf2
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-02-07 19:46:18 +0400

    save and push

--- machine_learning/ml-exercises-mlclass-ex1-004/py_ex1/main.py
+++ machine_learning/ml-exercises-mlclass-ex1-004/py_ex1/main.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import numpy
+from numpy import matrix
 
 
 def warm_up_exercise():
@@ -8,8 +9,11 @@ def warm_up_exercise():
 
 
 def load(filename):
-    return numpy.genfromtxt(filename, delimiter=',')
+    tmp = numpy.genfromtxt(filename, delimiter=',')
+    tmp = numpy.matrix.transpose(tmp)
+    return tmp
 
 if __name__ == '__main__':
     data = load('mlclass-ex1/ex1data1.txt')
+    print data[:1]
 
