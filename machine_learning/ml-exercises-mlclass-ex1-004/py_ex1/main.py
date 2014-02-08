5f156b80448f11dbc7bfba3e12764e50d1a404f7
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-02-08 11:53:15 +0400

    Compute error

--- machine_learning/ml-exercises-mlclass-ex1-004/py_ex1/main.py
+++ machine_learning/ml-exercises-mlclass-ex1-004/py_ex1/main.py
@@ -25,6 +25,15 @@ def plot_data(x, y):
     pylab.show()
 
 
+def compute_cost(x, y, theta):
+    j = 0
+    for i, elem in enumerate(x):
+        h_i = numpy.sum(elem*theta)
+        j += (h_i - y[i])**2
+    j *= 1.0 / (2 * len(x))
+    return j
+
+
 def main():
     data = load('mlclass-ex1/ex1data1.txt')
     x = data[:, :1]
@@ -32,8 +41,8 @@ def main():
     #plot_data(x, y)
     m = len(y)
     x = numpy.hstack([numpy.ones((m, 1)), x])
-    theta = numpy.zeros(())
-    print x
+    theta = numpy.zeros((1, 2))
+    j = compute_cost(x, y, theta)
 
 if __name__ == '__main__':
     main()
